9066d91f69fe1d41195971d026d8b7903479c920
Christopher Owen <christopherowen@users.noreply.github.com>
2025-12-17 04:24:31 +0100

    get bench data on start

--- bin/docker_start-gpt-oss-120b.sh
+++ bin/docker_start-gpt-oss-120b.sh
@@ -6,4 +6,6 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 # Repo root = one level up from that directory
 ROOT_DIR="$(cd -- "$SCRIPT_DIR/.." && pwd)"
 
+$ROOT_DIR/bin/get_bench_data.sh
+
 docker compose -f $ROOT_DIR/docker-compose-gpt-oss-120b.yml up -d vllm
